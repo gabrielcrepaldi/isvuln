a3852f32272ed75bb28e8f4c872275c279107f3d
crpld <bielcrepaldi07@gmail.com>
2026-06-18 13:04:44 +0000

    Add new feature - e-mail notification when a new finding is assigned to an analyst/admin

---
+++ lang/en/mail.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'assigned_subject' => 'A vulnerability has been assigned to you',
+    'greeting'         => 'Hello :name,',
+    'assigned_intro'   => 'The following vulnerability has been assigned to you:',
+    'view_button'      => 'View vulnerability',
+    'outro'            => 'Please review it as soon as possible.',
+];
---
+++ lang/pt_BR/mail.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'assigned_subject' => 'Uma vulnerabilidade foi atribuída a você',
+    'greeting'         => 'Olá :name,',
+    'assigned_intro'   => 'A seguinte vulnerabilidade foi atribuída a você:',
+    'view_button'      => 'Ver vulnerabilidade',
+    'outro'            => 'Por favor, analise-a o mais breve possível.',
+];
